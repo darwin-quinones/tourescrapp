a7e16859dd954da6c3741abe8a2e19fbef29336d
cdviloriama <charlie.viloria@hotmail.com>
2022-05-10 14:51:18 -0500

    First Commit Dev

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from users.models import User, Hotel
-# Register your models here.
+# Register your models here.    !
 admin.site.register(User)
 admin.site.register(Hotel)
